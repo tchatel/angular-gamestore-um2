8b3b278468cf22796dd95f46a78fbe5f2fa82283
Thierry Chatel <tchatel@gmail.com>
2013-11-19 19:54:07 +0100

    step-04

--- app/js/filters.js
+++ app/js/filters.js
@@ -2,27 +2,5 @@
 
 /* Filters */
 
-angular.module('gamestore.filters', [])
+angular.module('gamestore.filters', []);
 
-    .filter('interval', function ($parse) {
-
-        return function (input, property, min, max) {
-            if (!input) {
-                return undefined;
-            }
-
-            var get = $parse(property);
-
-            var output = [];
-            for (var i = 0 ; i < input.length ; i++) {
-                var item = input[i];
-                var okMin = !min || get(item) >= parseInt(min);
-                var okMax = !max || get(item) <= parseInt(max);
-                if (okMin && okMax) {
-                    output.push(item);
-                }
-            }
-            return output;
-        };
-
-    })
