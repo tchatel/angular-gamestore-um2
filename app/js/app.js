bb2d5fd9146099bb8f85d099ec5809072f5d79e6
Thierry Chatel <tchatel@gmail.com>
2013-11-19 19:50:01 +0100

    step-06

--- app/js/app.js
+++ app/js/app.js
@@ -8,7 +8,6 @@ angular.module('gamestore', ['ngRoute', 'ngSanitize',
 
         $routeProvider.when('/catalog',   {templateUrl: 'partials/catalog.html', controller: 'CatalogCtrl'});
         $routeProvider.when('/game/:ref', {templateUrl: 'partials/game.html',    controller: 'GameCtrl'});
-        $routeProvider.when('/cart',      {templateUrl: 'partials/cart.html',    controller: 'CartCtrl'});
 
         $routeProvider.otherwise({redirectTo: '/catalog'});
 
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,13 +4,6 @@
 
 angular.module('gamestore.controllers', ['gamestore.services'])
 
-    .controller('MainCtrl', function ($scope, $location, cart) {
-        $scope.addCart = function (game) {
-            cart.add(game);
-            $location.url("/cart");
-        }
-    })
-
     .controller('CatalogCtrl', function ($scope, $http, search) {
         $scope.search = search;
         $http.get("data/catalog.json").success(function (data) {
