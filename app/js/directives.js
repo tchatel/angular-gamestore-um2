0fddd1c6621193aadb284123b98ab0bcfdcf2367
Thierry Chatel <tchatel@gmail.com>
2014-11-11 19:25:05 +0100

    utilisation de $watchGroup

--- app/js/directives.js
+++ app/js/directives.js
@@ -10,25 +10,17 @@ angular.module('gamestore.directives', [])
             priority: 0,
             scope: false,
             link: function (scope, element, attrs) {
-                var stars = 0, maxStars = 0;
-                scope.$watch(attrs.stars, function (value) {
-                    stars = value;
-                    refresh();
-                });
-                scope.$watch(attrs.maxStars, function (value) {
-                    maxStars = value;
-                    refresh();
-                });
-                function refresh() {
+                scope.$watchGroup([attrs.stars, attrs.maxStars], function (value) {
+                    var stars = value[0], max = value[1];
                     var html = '';
                     for (var i = 0 ; i < stars ; i++) {
                         html += '<img src="img/star.png"/>';
                     }
-                    for (; i < maxStars ; i++) {
+                    for (; i < max ; i++) {
                         html += '<img src="img/empty-star.png"/>';
                     }
                     element.html(html);
-                }
+                });
             }
         }
     });
